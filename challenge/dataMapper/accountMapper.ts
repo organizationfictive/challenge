8938fc71be9efc3c6b32a6a63a8b49e0f760a7ec
responsiveObject <sgelinas@gmail.com>
2015-01-22 20:07:12 -0500

    fix accountMapper

--- challenge/dataMapper/accountMapper.ts
+++ challenge/dataMapper/accountMapper.ts
@@ -1,9 +1,11 @@
 ﻿/// <reference path="../models/accountModel.ts" />
+/// <reference path="../models/model.common.ts" />
+/// <reference path="../models/accountModel.ts" />
 module dataMapper {
     export class accountMapper implements IDtoMapper {
-        fromDto(dto:any, model?:model.IModel): model.IModel {
-            model = model || new model.accountModel(dto.username);
-            return model;
+        fromDto(dto:any, modelInstance?:model.IModel): model.IModel {
+            modelInstance = modelInstance || new model.accountModel(dto.username);
+            return modelInstance;
         }
         toDto(): string {
             return null;
